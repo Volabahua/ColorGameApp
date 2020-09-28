093762a82da840ed75dd4e0939facec46514c46a
Vlabakhua <volabahua@gmail.com>
2020-09-29 02:18:38 +0300

    # - new project

---
+++ ColorGame/ColorGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorGame
+//
+//  Created by Владимир Лабахуа on 29.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
